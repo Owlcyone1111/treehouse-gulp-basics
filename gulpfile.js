c25756349d28241e4bf158c078dc66c966a49782
Owlcyone1111 <samsam5886@hotmail.com>
2016-07-06 23:43:20 -0700

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'), // To minify
 	rename = require('gulp-rename'), // For rename function to work when minifying
 	sass = require('gulp-sass'), // To compile SASS into CSS
-	maps = require('gulp-sourcemaps');
+	maps = require('gulp-sourcemaps'),
+	del = require('del');
 
 gulp.task("hello", function() {
 	console.log("Hello!"); // gulp hello to run this task
@@ -21,6 +22,7 @@ gulp.task("concatScripts", function() {
 	.pipe(gulp.dest("js")); // Pipe into js directory
 });
 
+// ['concatScripts'] is a dependency to run tasks serially
 gulp.task("minifyScripts", ["concatScripts"], function() {
 	return gulp.src("js/app.js")
 	.pipe(uglify())
@@ -36,14 +38,29 @@ gulp.task('compileSass', function() {
 	.pipe(gulp.dest('css'));
 });
 
-gulp.task('watchSass', function() {
+gulp.task('watchFiles', function() {
 	gulp.watch('scss/**/*.scss', ['compileSass']); // Globbing pattern, look in scss folder, all subdirectories, and all files with .scss extension
+	gulp.watch('js/main.js', ['concatScripts']); // Watch main.js for changes and run concatScripts task when changed 
 });
 
-// Gulp runs tasks concurrently at the same time, but we need concat before minify, so add dependency between concat and minify and return statements to run tasks serially
-gulp.task("build", ['concatScripts', 'minifyScripts', 'compileSass']);
+// Gulp runs tasks concurrently at the same time, but we need concat before minify, so add dependency between concat and minify and return statements for tasks that depend on them, to run tasks serially. 'base' keeps the directory structure, otherwise it will dump all production files into 'dist' folder
+gulp.task("build", ['concatScripts', 'minifyScripts', 'compileSass'], function() {
+	return gulp.src(["css/application.css", 
+		"js/app.min.js", 
+		"index.html", 
+		"img/**", 
+		"fonts/**"], { base: './' })
+	.pipe(gulp.dest('dist'));
+});
 
- // Dependencies go in bracket, which will be ran first before default
-gulp.task("default", ['build'], ["hello"], function() {
-	console.log("This is the default task!");
+// Clean task to make sure nothing stays from past builds, especially for renamed files since new ones won't overwrite them
+gulp.task('clean', function() {
+	del(['dist', 'css/application.css*', 'app*.js*']);
+});
+
+gulp.task('serve', ['watchFiles']); // Initiates watchFiles task whenever we use the gulp serve command, can see changes made to Sass files right away
+
+ // Default task. Dependencies go in bracket, which will be ran first before default
+gulp.task("default", ['clean', 'hello'], function() {
+	gulp.start('build'); // Added clean as dependency, so build will run after clean has finished
 });
